40f04b46f3a8ebce9a5f50c4b00c0a05c2916e58
vitorsorpile <vitorsorpile@gmail.com>
2022-06-15 21:51:12 -0300

    Gerador funciona para equacoes de 1, 2 e 3 grau

--- Trabalho_02/Gerador.py
+++ Trabalho_02/Gerador.py
@@ -3,27 +3,64 @@ from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.template import Template
 from spade.message import Message
+from math import prod
 import random
 
 from main import email_gerador, senha_gerador
 
 class Gerador(Agent):
-   x = random.randint(-1000,1000)
-   a=0
-   while a == 0:
-      a = random.randint(-100,100)
-   y = -1 * (a*x)
+   
+   def gerarCoeficientes(self):
+      self.raizes = []
+      self.coeficientes = []
 
-   class funcao_1grau(CyclicBehaviour):
+      for _ in range(self.grauDaFuncao):
+         self.raizes.append(random.randint(-1000, 1000))
+   
+      print(self.raizes)
+      a = random.randint(-10, 10)
+      while (a == 0):
+         a = random.randint(-10, 10)
+
+      if self.grauDaFuncao == 1:
+         self.coeficientes.append(-1 * a * self.raizes[0])
+
+      else:
+         soma = sum(self.raizes)
+         produto = prod(self.raizes)
+
+         if self.grauDaFuncao == 2:
+            self.coeficientes.append(produto * a)
+            self.coeficientes.append(-1 * soma * a)
+         
+         elif self.grauDaFuncao == 3:
+            soma_dos_produtos = self.raizes[0] * self.raizes[1] + self.raizes[0] * self.raizes[2] + self.raizes[1] * self.raizes[2]
+
+            self.coeficientes.append(-1 * produto * a)
+            self.coeficientes.append(soma_dos_produtos * a)
+            self.coeficientes.append(-1 * soma * a)
+         
+      self.coeficientes.append(a)
+
+   def calcularResposta(self, x):
+      resposta = 0
+      i = 0
+      for coeficiente in self.coeficientes:
+         resposta += coeficiente*(x**i)
+         i += 1
+
+      return resposta
+
+   class responderValor(CyclicBehaviour):
       async def run(self):
          res = await self.receive(timeout=5)
          if res:
                x = float(res.body)
-               x = float( Gerador.a*x + Gerador.y )
-               print("Enviou para " + str(res.sender) + " f(",res.body,")= ",x,"=>",int(x))
+               y = float(self.agent.calcularResposta(x))
+               print("Enviou para " + str(res.sender) + " f(",res.body,")= ",y,"=>",int(y))
                msg = Message(to=str(res.sender)) 
                msg.set_metadata("performative", "inform")  
-               msg.body = str(int(x))
+               msg.body = str(int(y))
                await self.send(msg)
 
    class tipo_funcao(CyclicBehaviour):
@@ -32,18 +69,25 @@ class Gerador(Agent):
          if msg:
                msg = Message(to=str(msg.sender))
                msg.set_metadata("performative", "inform")
-               msg.body = "1grau" 
+               msg.body = f"{self.agent.grauDaFuncao}grau" 
                await self.send(msg)
                print("Respondeu para " + str(msg.sender) + " com " + msg.body)
                
 
    async def setup(self):
+      self.grauDaFuncao = random.randint(1, 3)
+
+      self.gerarCoeficientes()
+      print(f'Equacao do {self.grauDaFuncao} grau')
+      print(f'de coeficientes: {self.coeficientes}')
+      self.x = random.randint(-1000, 1000)
+
       t = Template()
       t.set_metadata("performative","subscribe")
 
-      tf = self.funcao_1grau()
-      print("Funcao de 1o grau: ", Gerador.x)
-      print("Funcao: ", Gerador.a, "x + (", Gerador.y, ")")
+      tf = self.responderValor()
+      # print("Funcao de 1o grau: ", Gerador.x)
+      # print("Funcao: ", Gerador.a, "x + (", Gerador.y, ")")
 
       self.add_behaviour(tf,t)
 
