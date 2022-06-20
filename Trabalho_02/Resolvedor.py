22ca573de3625e9d0376c279fb91c49562fecbd6
vitorsorpile <vitorsorpile@gmail.com>
2022-06-19 22:26:23 -0300

    Resolvedor para equacoes de 1 grau

--- Trabalho_02/Resolvedor.py
+++ Trabalho_02/Resolvedor.py
@@ -1,5 +1,5 @@
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour, FSMBehaviour, State
+from spade.behaviour import FSMBehaviour, State
 from spade.template import Template
 from spade.message import Message
 import random
@@ -8,8 +8,9 @@ import time
 from main import email_resolvedor, senha_resolvedor, email_gerador
 
 INITIAL_STATE = 'INITIAL_STATE'
-ANSWER_STATE = 'ANSWER_STATE'
 CALCULATE_STATE = 'CALCULATE_STATE'
+ANSWER_STATE = 'ANSWER_STATE'
+SOLVE_FIRST_GRADE_STATE = 'SOLVE_FIRST_GRADE_STATE'
 
 class Resolvedor(Agent):
 
@@ -25,51 +26,122 @@ class Resolvedor(Agent):
 
    class InitialState(State):
       async def run(self):
-         print("I'm at state one (initial state)")
+         print("I'm at the initial state")
          msg = Message(to=email_gerador)
          msg.set_metadata('performative', 'request')
+         
          await self.send(msg)
+         timeoutCounter = 0
 
          res = await self.receive(timeout=5)
-         if res:
-            print(f'Tenho que resolver uma equacao do tipo {res.body}')
-            # Set a first value to try to answer
-            self.agent.answer = random.randint(-1000, 1000)
-            self.set_next_state(ANSWER_STATE)
+
+         while not res:
+            res = await self.receive(timeout=5)
+            timeoutCounter += 1
+            if (timeoutCounter >= 5):
+               print('Parando agente após não ser respondido...')
+               await self.agent.stop()
+
+         print(f'Tenho que resolver uma equacao do tipo {res.body}')
+         self.agent.tipoDaFuncao = res.body
+
+         if (res.body == '1grau'):
+            self.set_next_state(SOLVE_FIRST_GRADE_STATE)
+         
+         else:
+            self.set_next_state(CALCULATE_STATE)
+
 
    class AnswerState(State):
       async def run(self):
-         print("I'm at answer state ")
+         msg = self.agent.msg
 
-         msg = Message(to=email_gerador)
-         msg.set_metadata('performative', 'subscribe')
-         msg.body = str(self.agent.answer)
+         print(f'Sending {msg.body} to {msg.to}')
          await self.send(msg)
 
-         msg = await self.receive(timeout=5)
-         if msg.body == '0':
-            print(f'Found solution!')
+         timeoutCounter = 0
+
+         res = None
+         res = await self.receive(timeout=5)
+
+         while not res:
+            res = await self.receive(timeout=5)
+            timeoutCounter += 1
+            if (timeoutCounter >= 5):
+               print('Parando agente após não ser respondido...')
+               await self.agent.stop()
+         
+    
+         if res.body == '0':
+            print(f'Found solution: {msg.body}')
             await self.agent.stop()
 
-         else:
-            print('Essa nao era a solucao')
-            self.set_next_state(CALCULATE_STATE)
+         if self.agent.tipoDaFuncao == '1grau':
+            self.agent.coeficientes.append(int(res.body))
+            self.set_next_state(SOLVE_FIRST_GRADE_STATE)
 
-   class CalculateState(State):
+
+   class SolveFirstGradeEquationState(State):
       async def run(self):
-         self.agent.answer = random.randint(-1000, 1000)
+         if len(self.agent.coeficientes) == 0:
+            self.agent.msg.body = '0'
+            self.set_next_state(ANSWER_STATE)
+            return
+
+         if len(self.agent.coeficientes) == 1:
+            self.agent.msg.body = '1'
+            self.set_next_state(ANSWER_STATE)
+            return
          
+         # Caso o valor retornado na terceira execução não seja zero => não foi possível encontrar a solução
+         if len(self.agent.coeficientes) > 2:
+            print('Ihh, alguma coisa deu errado. Não foi possível encontrar uma solução.')
+            await self.agent.stop()
+
+         b = int(self.agent.coeficientes[0])
+
+         a = int(self.agent.coeficientes[1]) - b
+         print(f'a: {a}')
+      
+         x = int(-b/a)
+         print(f'x: {x}')
+
+         self.agent.msg.body = str(x)
          self.set_next_state(ANSWER_STATE)
 
 
+   class CalculateState(State):
+      async def run(self):
+         msg = Message(to=email_gerador)
+         msg.set_metadata('performative', 'subscribe')
+
+         
+         while(True):
+            msg.body = str(random.randint(-1000, 1000))
+            print(f'Sending {msg.body} to {msg.to}')
+            await self.send(msg)
+
+            res = await self.receive(timeout=5)
+            if res.body == '0':
+               print(f'Found solution: {msg.body}')
+               await self.agent.stop()
+
+
    async def setup(self):
+      self.coeficientes = []
+
+      self.msg = Message(to=email_gerador)
+      self.msg.set_metadata('performative', 'subscribe')
+
       stateMachine = FSMBehaviour()
       stateMachine.add_state(name=INITIAL_STATE, state=self.InitialState(), initial=True)
-      stateMachine.add_state(name=ANSWER_STATE, state=self.AnswerState())
+      stateMachine.add_state(name=SOLVE_FIRST_GRADE_STATE, state=self.SolveFirstGradeEquationState())
       stateMachine.add_state(name=CALCULATE_STATE, state=self.CalculateState())
-      stateMachine.add_transition(source=INITIAL_STATE, dest=ANSWER_STATE)
-      stateMachine.add_transition(source=ANSWER_STATE, dest=CALCULATE_STATE)
-      stateMachine.add_transition(source=CALCULATE_STATE, dest=ANSWER_STATE)
+      stateMachine.add_state(name=ANSWER_STATE, state=self.AnswerState())
+      stateMachine.add_transition(source=INITIAL_STATE, dest=SOLVE_FIRST_GRADE_STATE)
+      stateMachine.add_transition(source=INITIAL_STATE, dest=CALCULATE_STATE)
+      stateMachine.add_transition(source=SOLVE_FIRST_GRADE_STATE, dest=ANSWER_STATE)
+      stateMachine.add_transition(source=ANSWER_STATE, dest=SOLVE_FIRST_GRADE_STATE)
 
       t = Template()
       t.set_metadata("performative","inform")
@@ -77,9 +149,7 @@ class Resolvedor(Agent):
       self.add_behaviour(stateMachine, t)
 
 
-#      tf = self.resposta()
- #     self.add_behaviour(tf,t)
-
+[-369, 992, 886]
 
 resolvedor = Resolvedor(email_resolvedor, senha_resolvedor)
 #resolvedor.web.start(hostname="127.0.0.1", port="10000")
